efdc8adcb526e0ce191a9a0b25f4d8aad943a23f
12core1 <12533753+visnkmr@users.noreply.github.com>
2023-12-16 17:29:46 +0530

    update to latest release of visnkmr/wfm_wrtc

--- src/components/sow.tsx
+++ src/components/sow.tsx
@@ -90,38 +90,40 @@ export const dlfd =(m)=>{
   }
 }
 export default function Sow(){
-    const [sdp, setSdp] = useState('')
-    // const [channel, setchannel] = useState<Ably.Types.RealtimeChannelPromise>()
-    // var channel:Ably.Types.RealtimeChannelPromise;
-    // const [offer, setoffer] = useState('')
-    // const [answer, setanswer] = useState('')
-    // const [amitheinitiator, setinitiator] = useState(true)
-    // const [peer, setp] = useState<Peer>(null)
-    var savepeer=useRef();
-    var showornot=useRef(false)
-    var [readyforconn,setr]=useState(false)
-    var [readytosend,setrts]=useState(false)
-    var [joinoth,setjoth]=useState(false)
-    var peer:Peer=savepeer.current;
-    var saveui4=useRef("");
-    // var ui4=saveui4.current;
-    const [showtext, setshowtext] = useState("")
-  //  dlfd(peer)
-    const startconn=(amitheinitiator)=>{
-      // useEffect(()=>{
-      return new Peer({
-      initiator: amitheinitiator,
-      trickle: false,
-      // wrtc: wrtc
-      // wrtc:nodeDatachannelPolyfill
-    })
-      // setp(p)
-    // },[amitheinitiator])
-  }
-  
-        // runonce.current=true;
-      // }
-    // },[])
+  const [sdp, setSdp] = useState('')
+  // const [channel, setchannel] = useState<Ably.Types.RealtimeChannelPromise>()
+  // var channel:Ably.Types.RealtimeChannelPromise;
+  // const [offer, setoffer] = useState('')
+  // const [answer, setanswer] = useState('')
+  // const [amitheinitiator, setinitiator] = useState(true)
+  // const [peer, setp] = useState<Peer>(null)
+  var savepeer=useRef();
+  var showornot=useRef(false)
+  var [readyforconn,setr]=useState(false)
+  var [readytosend,setrts]=useState(false)
+  var [joinoth,setjoth]=useState(false)
+  var peer:Peer=savepeer.current;
+  var saveui4=useRef("");
+  // var ui4=saveui4.current;
+  const [showtext, setshowtext] = useState("")
+//  dlfd(peer)
+  const startconn=(amitheinitiator)=>{
+    // useEffect(()=>{
+    return new Peer({
+    initiator: amitheinitiator,
+    trickle: false,
+    objectMode:true,
+    
+    // wrtc: wrtc
+    // wrtc:nodeDatachannelPolyfill
+  })
+    // setp(p)
+  // },[amitheinitiator])
+}
+
+      // runonce.current=true;
+    // }
+  // },[])
 
 // const setupchannel=()=>{
 //   dlfd("setup channel name "+ui4)
@@ -135,61 +137,61 @@ export default function Sow(){
 
 //   // if(offer.trim().length!==0 ){
 //     dlfd("setting offer on redis")
-    // useEffect(()=>{
-      const setofferdata = async (recdata) => {
-        // if(!onDataHandlerSetss){
-          let randomNumber = Math.floor(Date.now() % 10000);
-          console.log(randomNumber);
-          saveui4.current=randomNumber.toString();
-          dlfd(saveui4.current)
-          // dlfd(recdata)
-          await submittodb(saveui4.current, recdata);
-          showornot.current=true
-          // await getoffer()
-          // dlfd(uuidv4()); // Outputs a unique UUID
-          // const session = await kvstore.get(ui4);
-          // dlfd(session)
-          setshowtext(saveui4.current)
-          dlfd("initiator uid---->"+saveui4.current)
-          // dlfd(ably)
-          // onDataHandlerSetss = true;
-          //  try{
-            
-            
-          // }
-          // catch(e){
-          //   dlfd("FAILED")
-          //   dlfd(e)
-          // }
-            // // get the channel to subscribe to
-            // setupchannel()
-            // /*
-            //   Subscribe to a channel.
-            //   The promise resolves when the channel is attached
-            //   (and resolves synchronously if the channel is already attached).
-            // */
-            //subscribe to know when answers are being sent
-            // await channel.subscribe('answer', (message) => {
-            //   answerrecieved(message.data)
-            //   dlfd('Received a greeting message in realtime: ' + message.data)
-            // });
-          // ably.close()
+  // useEffect(()=>{
+    const setofferdata = async (recdata) => {
+      // if(!onDataHandlerSetss){
+        let randomNumber = Math.floor(Date.now() % 10000);
+        console.log(randomNumber);
+        saveui4.current=randomNumber.toString();
+        dlfd(saveui4.current)
+        // dlfd(recdata)
+        await submittodb(saveui4.current, recdata);
+        showornot.current=true
+        // await getoffer()
+        // dlfd(uuidv4()); // Outputs a unique UUID
+        // const session = await kvstore.get(ui4);
+        // dlfd(session)
+        setshowtext(saveui4.current)
+        dlfd("initiator uid---->"+saveui4.current)
+        // dlfd(ably)
+        // onDataHandlerSetss = true;
+        //  try{
+          
+          
         // }
-        // const data = await getData(1);
-        // setData(data);
-     }
-
-    //  const answerrecieved=(answer)=>{
-    //   dlfd("answerr recieved--------->"+answer)
-    //           peer.signal(JSON.parse(answer))
-      
-    //  }
-    
-    //  fetchData(offer);
-      // async ()=>{
+        // catch(e){
+        //   dlfd("FAILED")
+        //   dlfd(e)
+        // }
+          // // get the channel to subscribe to
+          // setupchannel()
+          // /*
+          //   Subscribe to a channel.
+          //   The promise resolves when the channel is attached
+          //   (and resolves synchronously if the channel is already attached).
+          // */
+          //subscribe to know when answers are being sent
+          // await channel.subscribe('answer', (message) => {
+          //   answerrecieved(message.data)
+          //   dlfd('Received a greeting message in realtime: ' + message.data)
+          // });
+        // ably.close()
       // }
-    // },[offer])
-  // }
+      // const data = await getData(1);
+      // setData(data);
+   }
+
+  //  const answerrecieved=(answer)=>{
+  //   dlfd("answerr recieved--------->"+answer)
+  //           peer.signal(JSON.parse(answer))
+    
+  //  }
+  
+  //  fetchData(offer);
+    // async ()=>{
+    // }
+  // },[offer])
+// }
 // }
 // //once decided on initiator
 // if(sdp.trim().length!==0)
@@ -207,8 +209,8 @@ export default function Sow(){
 //       catch(e){
 
 //       }
-      
-      
+    
+    
 //       // setshowtext(session)
 //  }
 
@@ -219,181 +221,264 @@ export default function Sow(){
 // //send answer to initiator
 // if(answer.trim().length!==0)
 // {useEffect(()=>{
-  const setanswerdata = async (answer) => {
-    await submittodb(saveui4.current, answer);
-    dlfd("sent answer to db")
-    setr(true)
-    console.log(readyforconn)
-    // setupchannel()
-        //ably send the answer over the connection
-    // await channel.publish('answer', answer);
+const setanswerdata = async (answer) => {
+  await submittodb(saveui4.current, answer);
+  dlfd("sent answer to db")
+  setr(true)
+  console.log(readyforconn)
+  // setupchannel()
+      //ably send the answer over the connection
+  // await channel.publish('answer', answer);
 
-      // setshowtext(session)
- }
+    // setshowtext(session)
+}
 const getoffer=async()=>{
-  console.log(saveui4.current)
-  var resp=await getfromdb(saveui4.current);
+console.log(saveui4.current)
+var resp=await getfromdb(saveui4.current);
 
 // Wait for the axios request to complete
 var response = await resp;
 
 // Access the data property of the response
-  var offer = response.data.data;
-  dlfd("got offer"+JSON.parse(offer))
-    peer.signal(JSON.parse(offer))
+var offer = response.data.data;
+dlfd("got offer"+JSON.parse(offer))
+  peer.signal(JSON.parse(offer))
 
 }
 const getanswer=async()=>{
-  console.log(saveui4.current)
-  var resp=await getfromdb(saveui4.current);
+console.log(saveui4.current)
+var resp=await getfromdb(saveui4.current);
 
 // Wait for the axios request to complete
 var response = await resp;
 
 // Access the data property of the response
-  var answer = response.data.data;
-  dlfd("answerr recieved--------->"+answer)
-  peer.signal(JSON.parse(answer))
-    // peer.signal(offer)
+var answer = response.data.data;
+dlfd("answerr recieved--------->"+answer)
+peer.signal(JSON.parse(answer))
+  // peer.signal(offer)
+
+}
+interface fileinfo{
+fileType:string,
+fileName:string,
+fileSize:number
 
 }
 //  fetchData();
 //   // async ()=>{
 //   // }
 // },[answer])}
-  // var onDataHandlerSet=false;
-  // useEffect(() => {
-    const initpeer=()=>{
+// var onDataHandlerSet=false;
+// useEffect(() => {
+  let receiveBuffer=[]
+  let receivedSize=0
+  const initpeer=()=>{
 
-      if (peer) {
-        // Check if 'data' event listener has already been set up
-        // if (!onDataHandlerSet) {
-          peer.on('error', err => dlfd('error'+ err))
-  
-          peer.on('signal', data => {
-            let recdata=JSON.stringify(data)
-            dlfd('SIGNAL'+ recdata)
-            // setshowtext(JSON.stringify(data))
-            if(data.type==="offer"){
-              dlfd("offer signal recieved")
-              // dlfd(recdata)
-              // setoffer(recdata)
-              // dlfd(offer)
-              setofferdata(recdata)
-            }else if(data.type==="answer"){
-              dlfd("answer signal recieved")
-              setanswerdata(recdata)
-              dlfd((peer))
-            }
-          })
-          peer.on('connect', () => {
-            dlfd('CONNECT')
-            setrts(true)
-
-            // ably.close()
-            
-          })
-          peer.on('data', data => {
-            dlfd(data)
-                      var gd=JSON.parse(data) as DataTypeDesc;
-                      if (gd.dataType === MessageTypeDesc.FILE) {
-                        dlfd("recieved file")
-                        download(gd.file || '', gd.fileName || "fileName", gd.fileType)
-                    }
-                    else{
-      
-                      dlfd('Received'+ gd.message);
-                      // dlfd('Received', JSON.stringify(gd));
-                      // setm("Friend : " + gd.message)
-                    }
-          })
-     
-          // Mark 'data' event listener as set up
-          // onDataHandlerSet = true;
-        // }
-      }
-    //  }, [peer]);
-    }
-    
-  const handleJoin=() => {
-    savepeer.current=startconn(false)
-    peer=savepeer.current
-    dlfd(JSON.stringify(peer))
-    initpeer()
-    // dlfd("offer got from kvstore----->"+sdp)
-    
-    saveui4.current=sdp
-    // setupchannel()
-    
-    getoffer()
-  }
-
-
-  const sendMessage=()=> {
-    dlfd("sending message")
-    // send message at sender or receiver side
     if (peer) {
-      let sm=(JSON.stringify(
-        {
-        dataType:MessageTypeDesc.OTHER,
-        message: 
-        'whatever' + Math.random()
+      // Check if 'data' event listener has already been set up
+      // if (!onDataHandlerSet) {
+        peer.on('error', err => dlfd('error'+ err))
 
-      } as DataTypeDesc))
-        // setm("Me : " + msg.value)
-        dlfd(sm)
-        peer.send(sm)
+        peer.on('signal', data => {
+          let recdata=JSON.stringify(data)
+          dlfd('SIGNAL'+ recdata)
+          // setshowtext(JSON.stringify(data))
+          if(data.type==="offer"){
+            dlfd("offer signal recieved")
+            // dlfd(recdata)
+            // setoffer(recdata)
+            // dlfd(offer)
+            setofferdata(recdata)
+          }else if(data.type==="answer"){
+            dlfd("answer signal recieved")
+            setanswerdata(recdata)
+            dlfd((peer))
+          }
+        })
+        peer.on('connect', () => {
+          dlfd('CONNECT')
+          setrts(true)
+
+          // ably.close()
+          
+        })
         
+        var e:fileinfo;
+        peer.on('data', data => {
+          if (typeof data.byteLength !== "undefined") {
+            let percentage = 0;
+            receiveBuffer.push(data);
+            receivedSize += data.byteLength;
+            percentage = ((receivedSize / e.fileSize) * 100).toFixed(3);
+            peer.send(
+              JSON.stringify({
+                type: "progress",
+                value: percentage
+              })
+            );
+            if (e.fileSize !== 0 && e.fileName) {
+              if (receivedSize == e.fileSize) {
+                const received = new Blob(receiveBuffer);
+                receiveBuffer = [];
+                download(received || '', e.fileName || "fileName", e.fileType)
+              }
+            }
+          } else {
+            try {
+              if (isJSON(data)) {
+                var sData = JSON.parse(data);
+    
+                if (sData.type === "progress") {
+                  console.log("progressing "+sData.value)
+                }
+                else if (sData.type === "fileinfo") {
+                  e=JSON.parse(sData.value)
+                }
+              }
+            } catch (error) {
+              console.log("TryCatch", error);
+            }
+          }
+        })
     }
+  //  }, [peer]);
+  }
+  const isJSON = str => {
+    try {
+      return JSON.parse(str) && !!str;
+    } catch (e) {
+      return false;
+    }
+  };
+  
+const handleJoin=() => {
+  savepeer.current=startconn(false)
+  peer=savepeer.current
+  dlfd(JSON.stringify(peer))
+  initpeer()
+  // dlfd("offer got from kvstore----->"+sdp)
+  
+  saveui4.current=sdp
+  // setupchannel()
+  
+  getoffer()
+}
+
+
+const sendMessage=()=> {
+  dlfd("sending message")
+  // send message at sender or receiver side
+  if (peer) {
+    let sm=(JSON.stringify(
+      {
+      dataType:MessageTypeDesc.OTHER,
+      message: 
+      'whatever' + Math.random()
+
+    } as DataTypeDesc))
+      // setm("Me : " + msg.value)
+      dlfd(sm)
+      peer.send(sm)
+      
+  }
 }
 const joinothenable=()=>{
-  setjoth(true)
+setjoth(true)
 }
 
 const [fileList, setFileList] = React.useState<[File]>([])
-    const [sendLoading, setSendLoading] = React.useState(false)
-    const handleUpload = async () => {
-        if(fileList){
+  const [sendLoading, setSendLoading] = React.useState(false)
+  const handleUpload = async () => {
+      if(fileList){
+    
+        if (fileList.length === 0) {
+            dlfd("Please select file")
+            return
+        }
+        dlfd(peer)
+        if (!peer) {
+            dlfd("Please select a connection")
+            return
+        }
+        try {
+            await setSendLoading(true);
+            let file = fileList[0] as unknown as File;
+            
+            sendData(file)
       
-          if (fileList.length === 0) {
-              dlfd("Please select file")
-              return
-          }
-          dlfd(peer)
-          if (!peer) {
-              dlfd("Please select a connection")
-              return
-          }
-          try {
-              await setSendLoading(true);
-              let file = fileList[0] as unknown as File;
-              let blob = new Blob([file], {type: file.type});
-        
-              await peer.send(JSON.stringify({
-                  dataType: MessageTypeDesc.FILE,
-                  file: blob,
-                  fileName: file.name,
-                  fileType: file.type
-              } as DataTypeDesc))
-              await setSendLoading(false)
-              dlfd("Send file successfully")
-          } catch (err) {
-              await setSendLoading(false)
-              dlfd(err)
-              dlfd("Error when sending file")
-          }
+            // await peer.send({
+            //   dataType: MessageTypeDesc.FILE,
+            //   file: blob,
+            //   fileName: file.name,
+            //   fileType: file.type
+            // })
+            await setSendLoading(false)
+            dlfd("Send file successfully")
+        } catch (err) {
+            await setSendLoading(false)
+            dlfd(err)
+            dlfd("Error when sending file")
         }
       }
-        const addfile=(event) => {
-          console.log(peer)
-          event.preventDefault();
-          const file = event.target.files[0];
-          if (file) {
-            setFileList([file]);
-          } else {
-            setFileList([]);
-          }
+    }
+      const addfile=(event) => {
+        console.log(peer)
+        event.preventDefault();
+        const file = event.target.files[0];
+        if (file) {
+          setFileList([file]);
+        } else {
+          setFileList([]);
         }
+      }
+      let sendBuffer = [];
+
+      const sendData = (file) => {
+        if (file.size === 0) {
+          dlfd("empty file")
+        }
+        const chunkSize = 32000;
+        var fileReader = new FileReader();
+        let offset = 0;
+        let fi:fileinfo={
+          fileType: file.type,
+          fileName: file.name,
+          fileSize: file.size
+        }
+        peer.send(JSON.stringify({
+          type:"fileinfo",
+          value:JSON.stringify(fi)
+        }))
+
+        sendBuffer=[]      
+        // wRTCTransferPaused = false;
+        fileReader.addEventListener("error", error =>
+          console.error("Error reading file:", error)
+        );
+        fileReader.addEventListener("abort", event =>
+          console.log("File reading aborted:", event)
+        );
+        fileReader.addEventListener("load", e => {
+          sendChunk(e.target.result);
+          offset += e.target.result.byteLength;
+          if (offset < file.size) {
+            readSlice(offset);
+          }
+        });
+        const readSlice = o => {
+          const slice = file.slice(offset, o + chunkSize);
+          fileReader.readAsArrayBuffer(slice);
+        };
+        readSlice(0);
+      };
+      const sendChunk = (data) => {
+        peer.send(data)
+        // if (wRTCTransferPaused) {
+        //   return;
+        // }
+    
+      };
   
     return (
       <div className='grid grid-flow-row'>
